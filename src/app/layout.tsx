a798e6ab76bc946dc8818f87cef0480c8ef7ada0
Nipunraj <nipunraj@mozilor.com>
2024-12-18 12:41:19 +0530

    working with noscript

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import Script from "next/script";
 const GTM_ID = "GTM-WWBB26M";
 
 const geistSans = Geist({
